844ad4ce46b9e9b3e471624a33e261e4e5e24a6f
Rotem-Lev-Lehman <Rotem12430@gmail.com>
2019-10-02 20:27:43 +0300

    A more efficient way to write to csv file

--- AdvandcedProjectionActionSelection/DependenciesPublishing/AdvancedProjectionDependeciesPublisher.cs
+++ AdvandcedProjectionActionSelection/DependenciesPublishing/AdvancedProjectionDependeciesPublisher.cs
@@ -10,12 +10,16 @@ namespace Planning
     class AdvancedProjectionDependeciesPublisher : AAdvancedProjectionActionPublisher
     {
         AAdvancedProjectionDependenciesSelector dependeciesSelector;
+
         private Dictionary<Agent, string> recordingDependencyPickingPerAgent;
-        private string recordingDependencyPickingAllTogether;
+        private string recordingDependencyPickingAllTogetherFilename;
         private Dictionary<Agent, int> currentDependencyRecordNumberPerAgent;
         private int currentDependencyRecordNumber;
         private static string togetherHeader = "Record number,Agent's name,Agent's record number,Action,Effect\n";
         private static string agentHeader = "Record number,Action,Effect\n";
+
+        private string recordingDependencyPickingAllTogetherData;
+        private Dictionary<Agent, string> recordingDependencyPickingDataPerAgent;
 
         public AdvancedProjectionDependeciesPublisher(AAdvancedProjectionDependenciesSelector dependeciesSelector, double percentageOfActionsSelected, Dictionary<Agent, string> recordingDependencyPickingPerAgent, string recordingDependencyPickingAllTogether) : base(percentageOfActionsSelected)
         {
@@ -24,11 +28,14 @@ namespace Planning
 
             //recording:
             this.recordingDependencyPickingPerAgent = recordingDependencyPickingPerAgent;
-            this.recordingDependencyPickingAllTogether = recordingDependencyPickingAllTogether;
+            this.recordingDependencyPickingAllTogetherFilename = recordingDependencyPickingAllTogether;
+            this.recordingDependencyPickingAllTogetherData = togetherHeader;
+            this.recordingDependencyPickingDataPerAgent = new Dictionary<Agent, string>();
             this.currentDependencyRecordNumberPerAgent = new Dictionary<Agent, int>();
             foreach(Agent agent in this.recordingDependencyPickingPerAgent.Keys)
             {
                 this.currentDependencyRecordNumberPerAgent.Add(agent, 0);
+                this.recordingDependencyPickingDataPerAgent.Add(agent, agentHeader);
             }
             this.currentDependencyRecordNumber = 0;
         }
@@ -51,7 +58,16 @@ namespace Planning
                 {
                     act.agent = agent.name;
                 }
+
+                WriteToFile(recordingDependencyPickingPerAgent[agent], recordingDependencyPickingDataPerAgent[agent]);
             }
+
+            WriteToFile(recordingDependencyPickingAllTogetherFilename, recordingDependencyPickingAllTogetherData);
+        }
+
+        private void WriteToFile(string filename, string data)
+        {
+            File.WriteAllText(filename, data);
         }
 
         private void InitializeEffectsWeCanReveal(List<Action> possibleActions, List<Tuple<Action, Predicate>> effectsWeCanReveal)
@@ -109,29 +125,20 @@ namespace Planning
             string effectChosen = privatePredicate.ToString();
 
             //Record to the agent's file:
-            string agentFilename = recordingDependencyPickingPerAgent[agent];
+            //string agentFilename = recordingDependencyPickingPerAgent[agent];
             int agentRecordNumber = currentDependencyRecordNumberPerAgent[agent];
             string agentData = agentRecordNumber + "," + actionChosen + "," + effectChosen + "\n";
-            AppendOrCreateFile(agentFilename, agentHeader, agentData);
+            recordingDependencyPickingDataPerAgent[agent] = recordingDependencyPickingDataPerAgent[agent] + agentData; //append next line
+            //AppendOrCreateFile(agentFilename, agentHeader, agentData);
 
             //Record to the all together file:
             string togetherData = currentDependencyRecordNumber + "," + Program.ConvertAgentNameToItsUsableName(agent) + "," + agentRecordNumber + "," + actionChosen + "," + effectChosen + "\n";
-            AppendOrCreateFile(recordingDependencyPickingAllTogether, togetherHeader, togetherData);
+            recordingDependencyPickingAllTogetherData = recordingDependencyPickingAllTogetherData + togetherData;
+            //AppendOrCreateFile(recordingDependencyPickingAllTogetherFilename, togetherHeader, togetherData);
 
             //Increase counters:
             currentDependencyRecordNumberPerAgent[agent]++;
             currentDependencyRecordNumber++;
-        }
-
-        private void AppendOrCreateFile(string filename, string headerLine, string data)
-        {
-            if (!File.Exists(filename))
-            {
-                //if the file doesn't exist yet, create it and write the header to it:
-                File.WriteAllText(filename, headerLine);
-            }
-            //write the data to the file:
-            File.AppendAllText(filename, data);
         }
     }
 }
